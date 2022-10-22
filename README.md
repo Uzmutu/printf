13fa1acbf9b0ae7691eee5ba764e81a8476624c9
Ousmane OUATTARA <ousmaneouattara@protonmail.com>
2022-10-22 00:25:58 +0000

    another test

--- README.md
+++
@@ -1,6 +0,0 @@
-# PRINTF PROJECT
-# Here is a team project in the framework of the ALX program,
-# where we will clone with our own code the "printf" function in C !
-# Contributors:
-# - Mahamadou Kheraba Diaby / Kepsondiaz(github username)
-# - Ousmane OUATTARA / Uzmutu(github username)
--- check_print.c
+++ check_print.c
@@ -1,38 +1,40 @@
 #include "main.h"
 /**
- * get_print - selects the right printing function
- * depending on the conversion specifier passed to _printf
- * @s: character that holds the conversion specifier
- * Description: the function loops through the structs array
- * func_arr[] to find a match between the specifier passed to _printf
- * and the first element of the struct, and then the approriate
- * printing function
- * Return: a pointer to the matching printing function
+ * handle_specifiers - Matches a conversion specifier with
+ * a corresponding conversion function.
+ * @char: character
+ * @int: int
+ *
+ * Return: If a conversion function is matched - a pointer to the function.
+ * Otherwise - NULL.
  */
-int (*get_print(char s))(va_list, flags_t *)
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
+		unsigned char, int, int, unsigned char)
 {
-	ph func_arr[] = {
-		{'i', print_int},
-		{'s', print_string},
-		{'c', print_char},
-		{'d', print_int},
-		{'u', print_unsigned},
-		{'x', print_hex},
-		{'X', print_hex_big},
-		{'b', print_binary},
-		{'o', print_octal},
-		{'R', print_rot13},
-		{'r', print_rev},
-		{'S', print_bigS},
-		{'p', print_address},
-		{'%', print_percent}
-		};
-	int flags = 14;
+	int i;
+	converter_t converters[] = {
+		{'c', convert_c},
+		{'s', convert_s},
+		{'d', convert_di},
+		{'i', convert_di},
+		{'%', convert_percent},
+		{'b', convert_b},
+		{'u', convert_u},
+		{'o', convert_o},
+		{'x', convert_x},
+		{'X', convert_X},
+		{'S', convert_S},
+		{'p', convert_p},
+		{'r', convert_r},
+		{'R', convert_R},
+		{0, NULL}
+	};
 
-	register int i;
+	for (i = 0; converters[i].func; i++)
+	{
+		if (converters[i].specifier == *specifier)
+			return (converters[i].func);
+	}
 
-	for (i = 0; i < flags; i++)
-		if (func_arr[i].c == s)
-			return (func_arr[i].f);
 	return (NULL);
 }
